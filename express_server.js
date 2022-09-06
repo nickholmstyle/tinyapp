15c6909e71da6c4d0946973a6735360fc1884a89
nickholmstyle <nick416holm@protonmail.com>
2022-09-06 18:57:38 +0000

    updating database and redirecting urls

--- express_server.js
+++ express_server.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = 8080;
 
 const generateRandomString = () => {
-  Math.random().toString(36).slice(2, 8)
+  return Math.random().toString(36).slice(2, 8)
 };
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -24,34 +26,32 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  const templateVars = { id, longURL };
   res.render('urls_show', templateVars)
 })
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`)
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`)
-});
-
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok")
+  // const longURL = req.body.longURL
+  const id = generateRandomString();
+  longURL = req.body.longURL
+  urlDatabase[id] = longURL
+  // console.log("+++++++++", id)
+  console.log(urlDatabase)
+
+  res.redirect(`/urls/${id}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
